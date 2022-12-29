1b8b3274f0dc0b881f23a7f7589c31eaa1abd952
BufferOverflow <scharxidev@outlook.de>
2022-12-29 21:18:15 +0100

    functionality to make request to external api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+
+async fn make_request(word: String) -> Result<String, reqwest::Error> {
+    let result = reqwest::get(format!("https://api.dictionaryapi.dev/api/v2/entries/en/{word}")).await?;
+
+    Ok(result.text().await?)
+}
+
+
+#[tokio::main]
+async fn main() -> reqwest::Result<()> {
+    match make_request("hello".to_owned()).await {
+        Ok(def) => println!("{}", def), 
+        Err(e) => return Err(e)
+    }; 
+
+    Ok(())
 }
